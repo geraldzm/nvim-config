3e626559678ce1be8378c59fc8b3bf540cd5fd36
gerald <geraldzmt@gmail.com>
2025-06-27 17:01:19 -0600

    format

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,21 +29,21 @@ return {
 	},
 
 	{
-	  "lukas-reineke/indent-blankline.nvim",
-	  event = "User FilePost",
-	  opts = {
-	    indent = { char = "│", highlight = "IblChar" },
-	    scope = { char = "│", highlight = "IblScopeChar" },
-	  },
-	  config = function(_, opts)
-	    dofile(vim.g.base46_cache .. "blankline")
+		"lukas-reineke/indent-blankline.nvim",
+		event = "User FilePost",
+		opts = {
+			indent = { char = "│", highlight = "IblChar" },
+			scope = { char = "│", highlight = "IblScopeChar" },
+		},
+		config = function(_, opts)
+			dofile(vim.g.base46_cache .. "blankline")
 
-	    local hooks = require "ibl.hooks"
-	    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-	    require("ibl").setup(opts)
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+			require("ibl").setup(opts)
 
-	    dofile(vim.g.base46_cache .. "blankline")
-	  end,
+			dofile(vim.g.base46_cache .. "blankline")
+		end,
 	},
 
 	{
